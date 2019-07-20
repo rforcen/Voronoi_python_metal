47b172009535b35acd1ab37fbadd7b9fd6a7768c
roberto forcen <robertoforcen@gmail.com>
2019-07-20 12:42:47 +0200

    Add files via upload

---
+++ voronoi/voronoi.py
@@ -0,0 +1,106 @@
+'''
+Voronoi tiles, python metal implementation
+'''
+from PyQt5.QtWidgets import QMainWindow, QLabel, QSizePolicy, QApplication, QDesktopWidget
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt
+
+import numpy as np
+import sys
+import runmetal
+
+
+class Voronoi():
+    def __init__(self, w, h):
+        self.w, self.h = w, h
+
+        n_points = min(w, h)
+        points = np.empty(shape=(n_points, 2), dtype=np.int32)  # set of x,y points in w,h range
+        points[:, 0] = np.random.randint(low=0, high=w, size=n_points, dtype=np.int32)
+        points[:, 1] = np.random.randint(low=0, high=h, size=n_points, dtype=np.int32)
+
+        self.pm, self.fn = None, None
+
+        self.init_metal()
+
+        self.pixelsBuf = self.pm.numpybuffer(np.empty(shape=(w, h), dtype=np.uint32))  # generate input MTL buffers
+        colorBuf = self.pm.numpybuffer(np.random.randint(low=0, high=0xffffff, size=n_points, dtype=np.uint32))
+        pointsBuf = self.pm.numpybuffer(points)
+        countBuf = self.intBuffer(n_points)
+
+        self.run_metal([self.pixelsBuf, pointsBuf, colorBuf, countBuf])
+
+    def getPixels(self):
+        return np.array(self.pm.buf2numpy(self.pixelsBuf, dtype=np.uint8).reshape(self.h, self.w, 4)[..., :3])
+
+    def intBuffer(self, i):
+        return self.pm.numpybuffer(np.array(i, dtype=np.int32))
+
+    def init_metal(self):
+        self.pm = runmetal.PyMetal()
+        self.pm.opendevice()
+        self.pm.openlibrary(filename='Voronoi.metal')
+        self.fn = self.pm.getfn("Voronoi")
+
+    def run_metal(self, parameters):
+        pm = self.pm
+        cqueue, cbuffer = pm.getqueue()
+
+        pm.runThread(cbuffer=cbuffer, func=self.fn, buffers=parameters,
+                     threads=({"width": self.w, "height": self.h, "depth": 1}))
+
+        pm.enqueue_blit(cbuffer, self.pixelsBuf)
+
+        pm.start_process(cbuffer)
+        pm.wait_process(cbuffer)
+
+
+class VoronoiWindow(QMainWindow):
+
+    def __init__(self, w, h):
+        super().__init__()
+        self.w, self.h = w, h
+        self.needsUpdate = True
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(10, 10, self.w, self.h)
+
+        self.pxlbl = QLabel()
+
+        self.setCentralWidget(self.pxlbl)
+        self.center()
+        self.show()
+
+    def center(self):
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+
+    def resizeEvent(self, event):
+        self.setWindowTitle(f'Voronoi tiles - METAL ({self.w:} x {self.h:})')
+        self.needsUpdate = True
+
+    def paintEvent(self, event):
+        def generate_pixmap():
+            pixels = Voronoi(self.w, self.h).getPixels()
+            qimage = QImage(pixels, pixels.shape[1], pixels.shape[0], QImage.Format_RGB888)
+            return QPixmap(qimage).scaled(self.w, self.h, Qt.KeepAspectRatio)
+
+        if self.needsUpdate:
+            self.needsUpdate = False
+            g = self.geometry()
+            self.w, self.h = g.width(), g.height()
+
+            self.pxlbl.setPixmap(generate_pixmap())
+
+
+def main():
+    app = QApplication(sys.argv)
+    _ = VoronoiWindow(1024, 768)
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
