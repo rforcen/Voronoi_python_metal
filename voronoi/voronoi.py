5c2e62f401aa3f471d42ad3e309fab5948ba9be2
roberto forcen <robertoforcen@gmail.com>
2019-07-20 12:44:55 +0200

    Add files via upload

--- voronoi/voronoi.py
+++ voronoi/voronoi.py
@@ -1,5 +1,37 @@
 '''
 Voronoi tiles, python metal implementation
+added to runmetal
+
+ def runThread(self, cbuffer, func, buffers, threads=None, label=None):
+        desc = Metal.MTLComputePipelineDescriptor.new()
+        if label is not None:
+            desc.setLabel_(label)
+        desc.setComputeFunction_(func)
+        state = self.dev.newComputePipelineStateWithDescriptor_error_(
+            desc, objc.NULL)
+        encoder = cbuffer.computeCommandEncoder()
+        encoder.setComputePipelineState_(state)
+        bufmax = 0
+        for i, buf in enumerate(buffers):
+            encoder.setBuffer_offset_atIndex_(buf, 0, i)
+            if bufmax < buf.length():
+                bufmax = buf.length()
+
+        # threads
+
+        # number of thread per group
+        w = state.threadExecutionWidth()
+        h = max(1, int(state.maxTotalThreadsPerThreadgroup() / w))
+        log.debug("w,h=%d,%d, bufmax=%d", w, h, bufmax)
+        tpg = self.getmtlsize({"width": w, "height": h, "depth": 1})
+
+        # number of thread per grig
+        ntg = self.getmtlsize(threads)
+        log.debug("threads: %s %s", ntg, tpg)
+
+        encoder.dispatchThreads_threadsPerThreadgroup_(ntg, tpg)
+        log.debug("encode(compute) %s", label)
+        encoder.endEncoding()
 '''
 from PyQt5.QtWidgets import QMainWindow, QLabel, QSizePolicy, QApplication, QDesktopWidget
 from PyQt5.QtGui import QPixmap, QImage
